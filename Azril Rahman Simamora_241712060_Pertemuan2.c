2458ee1dc2c12a9516128410b1641f805d3de8fe
Azril-rahman-s <simamoraazril16@gmail.com>
2024-09-25 09:31:52 +0700

    Add files via upload

---
+++ Azril Rahman Simamora_241712060_Pertemuan2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    printf("======================================== \n");
+    printf("               Biodata                   \n");
+    printf("======================================== \n");
+    printf("Nama            :Azril Rahman Simamora\n");
+    printf("Umur            : 17 tahun \n");
+    printf("Nim             : 241712060 \n");
+    printf("TGTL            : 2 juni 2006 \n");
+    printf("Hobi            : Menonton \n");
+    printf("Makanan Fav     : Mie \n");
+    printf("Minuman Fav     : Jus Alpukat \n");
+    printf("jenis Kelamin   : Laki-Laki\n");
+    printf("Sahabat         : Semua anak TI \n");
+    printf("Asal Sekolah    : Man 1 Medan \n");
+    printf("cita            : full stack\n");
+    printf("type-cewel      : cantik \n");
+    printf("");
+    return 0;
+}
